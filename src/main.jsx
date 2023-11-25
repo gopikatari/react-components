91328ddc22382a01d204d508b3a6dda96608e011
Gopi Katari <katarigopi@gmail.com>
2023-11-26 00:06:16 +0530

    commented the excluede

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,10 @@ import AppContext from '../context.jsx';
 import Reviews from './reviews/Review.jsx';
 import Accordion from './common/accordion/Accordion.jsx';
 import accData from './data/accordion-data.js';
-// import Menu from './common/menu/menu.jsx';
+import Menu from './common/menu/Menu.jsx';
 import Tab from './common/tabs/Tab.jsx';
 import Carousel from './common/carousel/Carousel.jsx';
-// import CarouselTest from './examples/slideEx/carouselTest.jsx';
+import CarouselTest from './examples/slideEx/CarouselTest.jsx';
 import GroceryBud from './examples/groceryBud/GroceryBud.jsx';
 import Navbar from './common/navbar/Navbar.jsx';
 
@@ -20,10 +20,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Navbar />
     <div className='container'>
       <AppContext>
+        <Menu />
         <Accordion data={accData} />
         <Tab />
         <Carousel />
-        {/* <CarouselTest /> */}
+        <CarouselTest />
         <GroceryBud />
       </AppContext>
     </div>
